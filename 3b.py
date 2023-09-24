b5a69df7ddccdffd13289f37056a5a04a29b796f
Chinmaya S C <120659062+Chinmaya003@users.noreply.github.com>
2023-09-24 21:36:21 +0530

    Update 3b.py

--- 3b.py
+++ 3b.py
@@ -1,7 +1,7 @@
 str1 = input("Enter String 1 \n")
 str2 = input("Enter String 2 \n")
 if len(str2) < len(str1):
- 	hort = len(str2)
+ 	short = len(str2)
  	long = len(str1)
 	else:
 		short = len(str1)
